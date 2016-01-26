75ff3767e5b50e59ee07731b558aa126bae4bd1c
Senthil <schellap@microsoft.com>
2016-01-25 17:36:29 -0800

    Package corehost

--- scripts/compile/compile-corehost.sh
+++ scripts/compile/compile-corehost.sh
@@ -30,6 +30,43 @@ if [[ "$OSNAME" == "osx" ]]; then
 else
    COREHOST_LIBNAME=libhostpolicy.so
 fi
-cp "$REPOROOT/src/corehost/cmake/$RID/cli/corehost" $HOST_DIR
-cp "$REPOROOT/src/corehost/cmake/$RID/cli/dll/${COREHOST_LIBNAME}" $HOST_DIR
+
+# Create nupkg for the host
+local PACKAGE_VERSION=$(cat "$REPOROOT/src/corehost/.version")
+read -d '' NUSPEC_CONTENTS <<"NUSPEC"
+<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2011/08/nuspec.xsd">
+    <metadata>
+        <id>Microsoft.DotNet.CoreHost</id>
+        <version>${PACKAGE_VERSION}</version>
+        <title>.NET CoreCLR Runtime Host</title>
+        <authors>Microsoft</authors>
+        <owners>Microsoft</owners>
+        <licenseUrl>http://go.microsoft.com/fwlink/?LinkId=329770</licenseUrl>
+        <projectUrl>https://github.com/dotnet/cli</projectUrl>
+        <iconUrl>http://go.microsoft.com/fwlink/?LinkID=288859</iconUrl>
+        <requireLicenseAcceptance>true</requireLicenseAcceptance>
+        <description>Provides the runtime host for .NET CoreCLR</description>
+        <releaseNotes>Initial release</releaseNotes>
+        <copyright>Copyright © Microsoft Corporation</copyright>
+    </metadata>
+</package>
+NUSPEC
+
+local PACKAGE_NAME=Microsoft.DotNet.CoreHost
+local PACKAGE_ROOT=$HOST_DIR/$PACKAGE_NAME
+local PACKAGE_NATIVE=$PACKAGE_ROOT/runtimes/$RID/native/
+mkdir -p $PACKAGE_NATIVE
+echo $NUSPEC_CONTENTS > $PACKAGE_ROOT/$COREHOST_PACKAGE_NAME.$COREHOST_VERSION.nuspec
+
+TARGET_DIRS=("$HOST_DIR" "$PACKAGE_NATIVE")
+for TARGET_DIR in "${TARGET_DIRS[@]}"
+do
+  cp "$REPOROOT/src/corehost/cmake/$RID/cli/corehost" $TARGET_DIR
+  cp "$REPOROOT/src/corehost/cmake/$RID/cli/dll/${COREHOST_LIBNAME}" $TARGET_DIR
+done
+
+zip -r $PACKAGE_NAME $PACKAGE_NAME
+mv $PACKAGE_NAME.zip $PACKAGE_NAME.nupkg
+
 popd 2>&1 >/dev/null
