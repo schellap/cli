289732c80c848664bff890899c451c3d30d02a9d
Senthil <schellap@microsoft.com>
2016-03-04 20:03:25 -0800

    Access modifier

--- src/corehost/cli/fx_muxer.h
+++ src/corehost/cli/fx_muxer.h
@@ -5,8 +5,9 @@ struct fx_ver_t;
 
 class fx_muxer_t
 {
-private:
+public:
     static int execute(const int argc, const pal::char_t* argv[]);
+private:
     static pal::string_t get_cli_version(const pal::string_t& global);
     static bool determine_sdk_dotnet_path(const pal::string_t& own_dir, pal::string_t* cli_sdk);
 };
